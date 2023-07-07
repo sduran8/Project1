d63858a4a2b5eab97b0d65762e49f0d3110ed052
user <sduran8@miners.utep.edu>
2023-07-07 15:46:30 -0600

    first commit

---
+++ toy-company/src/main/java/com/toycompany/toycompany/ToyCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.toycompany.toycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyCompanyApplication.class, args);
+	}
+
+}
---
+++ toy-company/src/test/java/com/toycompany/toycompany/ToyCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toycompany.toycompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
