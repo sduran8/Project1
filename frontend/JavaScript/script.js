aaa7661d54f0df7bcc5156fdb26eb801df0a4a2c
user <sduran8@miners.utep.edu>
2023-07-11 19:44:41 -0600

    Added a frontend folder

---
+++ frontend/JavaScript/script.js
@@ -0,0 +1,10 @@
+console.log("Hello, frontend!");
+
+const warehouseURL = 'http://localhost:8282/warehouses'
+const productURL = 'http://localhost:8282/products'
+const inventoryURL = 'http://localhost:8282/inventory'
+
+let warehouses = []
+let products = []
+let inventory = []
+
